b2d9ab596d2eb6b282dab0f5e68b1250c06cea90
Greg <gp.greg@gmail.com>
2018-01-06 12:14:51 +0000

    Add new method for RESTful API

--- index.py
+++ index.py
@@ -1,20 +1,21 @@
 #!/bin/bash/python3.5
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import getIpAddress
 import blinktControl
 
 app = Flask(__name__)
 
+# HTTP pages
 @app.route('/', methods=['GET'])
 def index():
 	if request.args.get('LedOn'):
-		led = int(request.args.get('number')) - 1
-		blinktControl.setLed(led)
+		led = int(request.args.get('number'))
+		blinktControl.setLed(led - 1)
 		status = "LED %d turned on" % led
 	elif request.args.get('LedOff'):
-		led = int(request.args.get('number')) - 1
-		blinktControl.clearLed(led)
+		led = int(request.args.get('number'))
+		blinktControl.clearLed(led - 1)
 		status = "LED %d turned off" % led
 	elif request.args.get('allOff'):
 		blinktControl.clearAll()
@@ -28,11 +29,21 @@ def index():
 
 	return render_template('index.html', status=status)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name = None):
-	return render_template('hello.html', name=name)
+# RESTful API
+@app.route('/blinkt', methods=['GET'])
+def blinkt_test():
+	return jsonify({'tasks': tasks})
 
+@app.route('/blinkt/<led>/<state>', methods=['GET'])
+def set_blinkt_state(led, state):
+	if int(led) in range(0, 8):
+		if int(state) == 0:
+			blinktControl.clearLed(int(led))
+		else:
+			blinktControl.setLed(int(led))
+	return jsonify() # Need to send response based on successful request
+
+# Local methods and runnint the app
 ipaddr = getIpAddress.IpAddress()
 print(ipaddr)
 
