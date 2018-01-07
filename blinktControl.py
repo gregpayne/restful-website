a003cc0c6bf3f9937a5aa570bfc4c5a456bc89a6
Greg <gp.greg@gmail.com>
2018-01-07 08:23:03 +0000

    Change API to be able set custom colors and brightness

--- blinktControl.py
+++ blinktControl.py
@@ -2,27 +2,38 @@
 
 import blinkt
 import time
+from colormap.colors import hex2rgb
 
-def setLed(led):
-	blinkt.set_brightness(0.1) # set the brightness between 0 and 1
-	blinkt.set_pixel(led, 255, 255, 255)
+def setLed(led, brightness, color):
+	blinkt.set_brightness(brightness) # set the brightness between 0 and 1
+	blinkt.set_pixel(led, getR(color), getG(color), getB(color))
 	blinkt.show()
 
 def clearLed(led):
-	blinkt.set_brightness(0.1) # set the brightness between 0 and 1
 	blinkt.set_pixel(led, 0, 0, 0)
 	blinkt.show()
 
-def setAll():
-	blinkt.set_brightness(0.1) # set the brightness between 0 and 1
+def setAll(brightness, color):
+	blinkt.set_brightness(brightness) # set the brightness between 0 and 1
 	for i in range(8):
-		blinkt.set_pixel(i, 255, 255, 255)
+		blinkt.set_pixel(i, getR(color), getG(color), getB(color))
 	blinkt.show()
 
 def clearAll():
-	blinkt.set_brightness(0.1) # set the brightness between 0 and 1
+	blinkt.set_brightness(brightness) # set the brightness between 0 and 1
 	blinkt.clear()
 	blinkt.show()
 
+def getR(color):
+	c = hex2rgb(color, normalise=False)
+	return c[0]
+
+def getG(color):
+	c = hex2rgb(color, normalise=False)
+	return c[1]
+
+def getB(color):
+	c = hex2rgb(color, normalise=False)
+	return c[2]
 
 
