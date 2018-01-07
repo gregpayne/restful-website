585503a4146b9a676d8cda445fcb85e3a99384f8
Greg <gp.greg@gmail.com>
2018-01-07 08:39:23 +0000

    Add brightness and color to defaults, hardcoded values to test blinktControl API

--- index.py
+++ index.py
@@ -7,14 +7,14 @@ import blinktControl
 app = Flask(__name__)
 
 blinkt = [
-	{'id': 0, 'color': 'FFFFFF', 'state': False },
-	{'id': 1, 'color': 'FFFFFF', 'state': False },
-	{'id': 2, 'color': 'FFFFFF', 'state': False },
-	{'id': 3, 'color': 'FFFFFF', 'state': False },
-	{'id': 4, 'color': 'FFFFFF', 'state': False },
-	{'id': 5, 'color': 'FFFFFF', 'state': False },
-	{'id': 6, 'color': 'FFFFFF', 'state': False },
-	{'id': 7, 'color': 'FFFFFF', 'state': False },
+	{'id': 0, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 1, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 2, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 3, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 4, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 5, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 6, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
+	{'id': 7, 'brightness': 0.1, 'color': '#FFFFFF', 'state': False },
 ]
 
 
@@ -52,7 +52,7 @@ def set_blinkt_state(led, state):
 		if int(state) == 0:
 			blinktControl.clearLed(int(led))
 		else:
-			blinktControl.setLed(int(led))
+			blinktControl.setLed(int(led), 1, '#FF0000')
 	return jsonify(blinkt) # Need to send response based on successful request
 
 # Local methods and runnint the app
