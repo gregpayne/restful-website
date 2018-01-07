37e9ae447c8b81e1aa37061a5c684ca4294952bf
Greg <gp.greg@gmail.com>
2018-01-07 13:04:21 +0000

    Change API for turning on LED to accepts a string instead of boolean for state

--- index.py
+++ index.py
@@ -52,7 +52,7 @@ def set_blinkt_state(led, state):
 		if int(state) == 0:
 			blinktControl.clearLed(int(led))
 		else:
-			blinktControl.setLed(int(led), 0.1, '#FF0000', True)
+			blinktControl.setLed(int(led), 0.1, '#FF0000', "true")
 	return jsonify(blinkt) # Need to send response based on successful request
 
 @app.route('/blinkt/<led_id>', methods=['PUT'])
